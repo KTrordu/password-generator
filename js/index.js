a8e4f8eea3d0ab64c22c017c550b4345540778be
Alptuğ Yılmaz <alptug.yilmaz@outlook.com>
2024-09-19 02:24:30 +0300

    Html, css and js update

--- js/index.js
+++ js/index.js
@@ -5,3 +5,39 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
     "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*",
     "(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+let passLengthEl = document.getElementById("input");
+const defaultPassLength = 15;
+
+function generatePass() {
+    
+    if (passLengthEl.value > 15) {
+        alert("Your password length can be at most 15.");
+        return;
+    }
+    
+    let pass1 = "";
+    let pass2 = "";
+
+    
+    let passLength = passLengthEl.value;
+    let passHolder;
+    let passCharacter;
+    
+    for (let i = 0; i < passLength; i++) {
+        passCharacter = Math.floor(Math.random() * characters.length);
+        passHolder = characters[passCharacter];
+
+        pass1 += passHolder;
+    }
+
+    for (let i = 0; i < passLength; i++) {
+        passCharacter = Math.floor(Math.random() * characters.length);
+        passHolder = characters[passCharacter];
+
+        pass2 += passHolder;
+    }
+
+    document.getElementById("pass1").innerHTML = pass1;
+    document.getElementById("pass2").innerHTML = pass2;
+}
